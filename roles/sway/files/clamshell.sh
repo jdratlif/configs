b3ed30fd44c5af7d42a118307ecf24fbaede4328
John Ratliff <jdratlif@globalnoc.iu.edu>
2024-01-24 13:30:05 +0000

    add clamshell config to sway

---
+++ roles/sway/files/clamshell.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# https://github.com/swaywm/sway/wiki#clamshell-mode
+
+if grep -q open /proc/acpi/button/lid/LID/state; then
+    swaymsg output <LAPTOP> enable
+else
+    swaymsg output <LAPTOP> disable
+fi
