863240dcf353f8badd9aec40d56bc4c0611e46fa
John Ratliff <jdratlif@globalnoc.iu.edu>
2024-02-02 22:37:29 +0000

    add devcontainer config

---
+++ .devcontainer/prompt.sh
@@ -0,0 +1,1 @@
+PS1="[\e[35m\]\u@devcontainer \e[34m\]\w\e[00m\]] (\e[33m\]\$(git branch --show-current 2>/dev/null)\e[00m\]) \$ "
---
+++ .devcontainer/root.bashrc
@@ -0,0 +1,2 @@
+source /etc/profile.d/bash_completion.sh
+source /etc/profile.d/prompt.sh
