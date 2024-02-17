589bd1ce98b97cf0d1d06aee177f533c578682a7
John Ratliff <jdratlif@globalnoc.iu.edu>
2024-02-17 21:36:49 +0000

    update git branch in prompt.sh

--- .devcontainer/prompt.sh
+++ .devcontainer/prompt.sh
@@ -1,1 +1,1 @@
-PS1="[\e[35m\]\u@devcontainer \e[34m\]\w\e[00m\]] (\e[33m\]\$(git branch --show-current 2>/dev/null)\e[00m\])\n\$ "
+PS1="[\e[35m\]\u@devcontainer \e[34m\]\w\e[00m\]] (\e[33m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')\e[00m\])\n\$ "
